dc6c55bfcf965b549872f632e85ad1b7d5133b44
jmlizarraga <jlizarraga@chilesystems.com>
2021-10-13 15:35:53 -0300

    beneficios sindrome de down

--- configuraciones/rutasProyecto.js
+++ configuraciones/rutasProyecto.js
@@ -3,11 +3,13 @@
 import { Inicio } from "../componentes/inicio/inicio.js";
 import { Beneficios } from "../componentes/beneficios/beneficios.js";
 import { Contacto } from "../componentes/contacto/contacto.js";
+import { BeneficiosSindromeDown } from "../componentes/beneficiosSindromeDown/beneficiosSindromeDown.js";
 
 
 export const rutasProyecto = [
     {nombre: "inicio", componente: Inicio, default: true},
     {nombre: "beneficios", componente: Beneficios},
+    {nombre: "beneficiosSindromeDown", componente: BeneficiosSindromeDown},
     {nombre: "contacto", componente: Contacto}
 ];
 
