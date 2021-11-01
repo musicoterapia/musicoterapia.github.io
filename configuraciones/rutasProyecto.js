872fc22e2763b130c98168000572549b6e7e197c
jmlizarraga <jlizarraga@chilesystems.com>
2021-11-01 09:26:52 -0300

    creatividad

--- configuraciones/rutasProyecto.js
+++ configuraciones/rutasProyecto.js
@@ -4,12 +4,14 @@ import { Inicio } from "../componentes/inicio/inicio.js";
 import { Beneficios } from "../componentes/beneficios/beneficios.js";
 import { Contacto } from "../componentes/contacto/contacto.js";
 import { BeneficiosSindromeDown } from "../componentes/beneficiosSindromeDown/beneficiosSindromeDown.js";
+import { Creatividad } from "../componentes/creatividad/creatividad.js";
 
 
 export const rutasProyecto = [
     {nombre: "inicio", componente: Inicio, default: true},
     {nombre: "beneficios", componente: Beneficios},
     {nombre: "beneficiosSindromeDown", componente: BeneficiosSindromeDown},
+    {nombre: "creatividad", componente: Creatividad},
     {nombre: "contacto", componente: Contacto}
 ];
 
